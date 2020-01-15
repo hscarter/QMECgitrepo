1a86350da6fb9c3e91e061f992f2f645747c066d
Haley Carter <hsharoncarter@gmail.com>
2020-01-14 18:17:24 -0600

    did homework1, created script and data

---
+++ homework1.R
@@ -0,0 +1,70 @@
+#Homework Assignment 1
+#by Haley Carter
+
+#make dataframe
+trial <- c(1:50) #there will be 50 trials
+trial <- sample(trial, 50) #random assigment requires randomly selecting from trial number
+assignment <- rep(T:F, each = 25) #first 25 randomly sampled trial numbers assigned treatment 1
+#treatment 1 is starting the shuffle with my right hand
+df <- data.frame(trial = trial, assignment = assignment) #combine them
+df <- df[order(df$trial),] #sort in order of trial number
+
+#record data, 1 means successfull shuffle
+results <- c(
+  0,
+  0,
+  1,
+  0,
+  1,
+  0,
+  0,
+  1,
+  1,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  1,
+  1,
+  0,
+  0,
+  1,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  0,
+  1,
+  1,
+  1,
+  1,
+  0,
+  1,
+  0,
+  1,
+  1,
+  0,
+  1,
+  1,
+  0,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  0,
+  1
+)
+#add results to the dataframe
+df$results <- results
+#save the data
+write.csv(df, "hw1_data.csv")
+
+#read this data in remotely
