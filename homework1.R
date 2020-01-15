55757b9255ce2f5ebf78af4819ccfce5e62408c5
Haley Carter <hsharoncarter@gmail.com>
2020-01-14 18:31:21 -0600

    Update homework1.R
    added report paragraph

--- homework1.R
+++ homework1.R
@@ -1,6 +1,12 @@
 #Homework Assignment 1
 #by Haley Carter
 
+#I am not very good at shuffling cards generally and I wanted to see if it made a difference
+#which hand I used to initiate the shuffle. The subject is my shuffling, the treatments are
+#starting with my right hand or my left hand. Treatment assignment was done randomly 
+#(see code below). And the response was a successful shuffle (all cards could be pushed together)
+#or an unsuccessfull shuffle (cards could not be combined after shuffling attempt).
+
 #make dataframe
 trial <- c(1:50) #there will be 50 trials
 trial <- sample(trial, 50) #random assigment requires randomly selecting from trial number
@@ -65,7 +71,7 @@ results <- c(
 #add results to the dataframe
 df$results <- results
 #save the data
-write.csv(df, "hw1_data.csv") #I'm working from a git repo, so this also saves online
+write.csv(df, "hw1_data.csv") #I'm working from a git repo so also saves online
 
 #read this data in remotely from github
 hw1_data <- read.csv("https://raw.githubusercontent.com/hscarter/QMECgitrepo/master/hw1_data.csv")
